c59e8145885b5f2ba33dec0b29556fe990a1308a
mamulaidze <mamulaidzeg186@gmail.com>
2024-12-07 01:50:01 +0400

    updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import Info from './Info';
 
 export default function Hero() {
   return (
-    <main className="container mx-auto">
+    <main className="container mx-auto shadow-">
       {/*//! Mobile version */}
       <div className="relative bg-hero-bg bg-no-repeat bg-center h-screen bg-cover bg-gray-900">
         <div className="flex flex-col items-center justify-center h-screen px-4 bg-gray-800 bg-opacity-80 text-center">
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './components/Header'
 import Hero from './components/Hero'
-
+import Foooter from './components/Foooter'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header/>
     <Hero/>
+    <Foooter/>
   </StrictMode>,
 )
